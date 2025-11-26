fa0b0862b6c8fb2639a47a44e2449c57294c1dae
gorelikova1993 <yulia.gorelikova1993@gmail.com>
2025-11-26 13:37:28 +0300

    3.16 finished

---
+++ src/interfaces/Main.java
@@ -0,0 +1,15 @@
+package interfaces;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        Client client = new Client();
+        client.makeOrder(new Waiter(){
+            @Override
+            public void bringOrder(String dish) {
+                System.out.println(dish);
+            }
+        }, "Пицца");
+    }
+}
